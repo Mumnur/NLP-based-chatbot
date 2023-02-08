65e364b27c9f4c97fc1338bd1d5bc8ae01951605
Mumnur <mumnur96@gmail.com>
2023-02-08 20:45:51 +0300

    second commit

---
+++ chatbot/chatbot.py
@@ -0,0 +1,89 @@
+from .conversation import *
+import os
+import re
+import codecs
+import logging
+
+logger = logging.getLogger(__name__)
+
+with open(os.path.join(os.path.dirname(__file__), "slurs-encoded.txt"), "r") as f:
+    slurs = f.read().splitlines()
+    slurs = map(lambda x: codecs.decode(x, "rot13"), slurs)
+    slurs = list(slurs)
+    slurs = "|".join(slurs)
+    slurs = re.compile(slurs)
+
+
+def has_slur(message: str):
+    return slurs.search(message.lower()) is not None
+
+
+class Chatbot(object):
+    def __init__(self, name: str, preamble: str = "", force_cpu: bool = False, **kwargs):
+        self.name = name
+        self.force_cpu = force_cpu
+        self.preamble = preamble
+
+        self._init_model(**kwargs)
+
+    def model_max_length(self) -> str:
+        return "\u221e"
+
+    def _init_model(self, **kwargs):
+        pass
+
+    def generate_response(self, convo: Conversation) -> str:
+        response = self._generate(convo)
+        if has_slur(response):
+            logger.info(f"Generated response containing slur: {response}")
+            return
+
+        if response != "":
+            convo.add_message(ChatbotMessage(self.name, response))
+
+        return response
+
+    def _generate(self) -> str:
+        pass
+
+
+class BruhChatbot(Chatbot):
+    def _generate(self, convo: Conversation) -> str:
+        return "Bruh"
+
+    def _generate_model_input(self, convo: Conversation) -> str:
+        return ""
+
+
+def test(**kwargs):
+    print("============== Chatbot Tester ==============")
+    name = input("Enter your name: ")
+    for i, subclass in enumerate(Chatbot.__subclasses__()):
+        print(f"{i}: {subclass.__name__}")
+
+    choice = -1
+    while choice < 0 or choice > len(Chatbot.__subclasses__()):
+        choice = int(input("Select a chatbot: "))
+
+    chatbot = Chatbot.__subclasses__()[choice](**kwargs)
+    conversation = Conversation("test", logdir="chatlogs")
+
+    print("Loaded Chatbot\n")
+
+    while True:
+        try:
+            message = input(f"{name}: ")
+            conversation.add_message(ChatbotMessage(name, message))
+            response = chatbot.generate_response(conversation)
+            print(f"{chatbot.name}: {response}")
+        except KeyboardInterrupt:
+            break
+
+    print("\n\n============ Summary ============")
+    # print(conversation.summary())
+    print(chatbot._generate_model_input(conversation))
+    conversation.reset()
+
+
+if __name__ == "__main__":
+    test(name="Bot")
---
+++ chatbot/transformer.py
@@ -0,0 +1,150 @@
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from .chatbot import *
+import arrow
+
+
+class TransformerSettings(NamedTuple):
+    model_name: str
+    temperature: float
+    top_p: float
+    top_k: int
+    repetition_penalty: float
+    max_outlen: int = 128
+
+
+gpt2 = TransformerSettings(
+    model_name="gpt2", temperature=1.0, top_p=0.9, top_k=None, repetition_penalty=1.33
+)
+
+gpt2Medium = TransformerSettings(
+    model_name="gpt2-medium", temperature=1.0, top_p=0.90, top_k=None, repetition_penalty=1.33
+)
+
+gpt2Large = TransformerSettings(
+    model_name="gpt2-large", temperature=1.0, top_p=0.9, top_k=None, repetition_penalty=1.33
+)
+
+gpt2XL = TransformerSettings(
+    model_name="gpt2-xl",
+    temperature=1.0,
+    top_p=0.9,
+    top_k=None,
+    repetition_penalty=1.33,
+)
+
+gptDistil = TransformerSettings(
+    model_name="distilgpt2", temperature=0.8, top_p=0.9, top_k=None, repetition_penalty=1.2
+)
+
+gptNeoSmall = TransformerSettings(
+    model_name="EleutherAI/gpt-neo-125M",
+    temperature=1.1,
+    top_p=0.9,
+    top_k=None,
+    repetition_penalty=1.2,
+)
+
+gptNeo = TransformerSettings(
+    model_name="EleutherAI/gpt-neo-1.3B",
+    temperature=1.1,
+    top_p=0.9,
+    top_k=None,
+    repetition_penalty=3.0,
+)
+
+
+# Sike it's actually distilgpt2
+class Transformer(Chatbot):
+    def _init_model(self, settings: TransformerSettings):
+        self.settings = settings
+
+        if torch.cuda.is_available():
+            self.gpu = True
+            self.model = AutoModelForCausalLM.from_pretrained(
+                settings.model_name,
+                device_map="auto",
+                # load_in_8bit=True,
+            )
+            self.tokenizer = AutoTokenizer.from_pretrained(settings.model_name)
+        else:
+            self.gpu = False
+            self.model = AutoModelForCausalLM.from_pretrained(settings.model_name)
+            self.tokenizer = AutoTokenizer.from_pretrained(settings.model_name)
+
+        self.model.eval()
+
+        if self.model.config.pad_token_id is None:
+            self.model.config.pad_token_id = self.model.config.eos_token_id
+
+        self.endline_token = self.tokenizer.encode("\n")[0]
+        self.model_eos_str = self.tokenizer.decode([self.model.config.eos_token_id])
+
+    def _generate_model_input(self, convo: Conversation) -> str:
+        out = self.preamble + "\n"
+        message: ChatbotMessage = None
+        for message in convo.queue:
+            out += (
+                f"[{arrow.get(message.timestamp).humanize()}]<{message.sender}>{message.message}\n"
+            )
+
+        out += f"[{arrow.utcnow().humanize()}]<{self.name}>"
+        return out
+
+    def model_max_length(self) -> str:
+        return str(self.tokenizer.model_max_length)
+
+    def _generate(self, convo: Conversation) -> str:
+        input_text = self._generate_model_input(convo)
+
+        while (
+            len(self.tokenizer.encode(input_text))
+            >= self.tokenizer.model_max_length - self.settings.max_outlen
+        ):
+            convo.dequeue()
+            input_text = self._generate_model_input(convo)
+
+        input_ids = self.tokenizer.encode(input_text, return_tensors="pt")
+        outputs = self.model.generate(
+            input_ids.cuda() if self.gpu else input_ids,
+            max_length=max(
+                len(input_ids[0]) + self.settings.max_outlen, self.tokenizer.model_max_length
+            ),
+            num_beams=1,
+            do_sample=False,
+            temperature=self.settings.temperature,
+            top_p=self.settings.top_p,
+            repetition_penalty=self.settings.repetition_penalty,
+            eos_token_id=self.endline_token,
+            pad_token_id=self.model.config.pad_token_id,
+            # exponential_decay_length_penalty=(10, 0.75),
+        )
+        output = self.tokenizer.decode(outputs[0])
+        output = output.split(self.model_eos_str)[0]
+
+        # print("\n==============================")
+        # print(output)
+        # print("==============================\n")
+
+        output = output[len(input_text) :].strip()
+        firstBracket = output.find("<")
+        firstClosing = output.find(">")
+
+        if firstBracket != -1 and firstClosing != -1:
+            output = output[:firstBracket]
+
+        return output
+
+
+preamble = """Amogus is our Lord and saviour.
+
+Hailing from a Sus village, Lord Amogus became so powerful and wise that
+Amogus was able to defeat Ultimate Sus by turning it into a suspicious Sus,
+thereby setting us free from the sus pain.
+
+From that day forward, we are not suspicious Sus, but suspension Sus.
+I am sus and you are sus. Defeat that stupid Ultimate Sus! - AMOGUS
+-----"""
+
+if __name__ == "__main__":
+    test(settings=gptNeo, name="AMOGUS", preamble=preamble)
